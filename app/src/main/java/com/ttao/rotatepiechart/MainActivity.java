e22fa89c2096a1ab73ebbd7e84a587495bdedea3
tengtao <1345580079@qq.com>
2017-11-03 11:44:13 +0800

    demo代码调整

--- app/src/main/java/com/ttao/rotatepiechart/MainActivity.java
+++ app/src/main/java/com/ttao/rotatepiechart/MainActivity.java
@@ -1,7 +1,6 @@
 package com.ttao.rotatepiechart;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -21,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         RotatePieChart pieChart = findViewById(R.id.pie_chart);
         TextView textView = findViewById(R.id.text);
 
+
+        //        pieChart.setBackgroundColor(Color.RED);
+        //        pieChart.setIndicatorAngle(20);
+        //        pieChart.setIndicatorHeight(PieChartUtils.dp2px(this, 50));
+        //        pieChart.setIndicatorColor(Color.GREEN);
+        //        pieChart.setEntranceAnimationDuration(200);
+        //        pieChart.setOffsetAnimationDuration(800);
+        //        pieChart.setOutsideStrokeWidth(PieChartUtils.dp2px(this, 20));
+        //        pieChart.setOutsideStrokeColor(Color.BLUE);
+        //        pieChart.setStartAngle(0);
+
+
         List<PieChartBean> list = new ArrayList<>();
 
         int[] colors = new int[]{0xFFFD9033, 0xFFF98AEC, 0xFFFF3564, 0xFFF566D4, 0xFF58F4Fa,
@@ -28,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 0xFFF490F3, 0xFF118AEC, 0xFFFFF564, 0xFFF56fD4, 0xFF5dFaFa,
                 0xFFFDF03A, 0xFF000000, 0xFF00FF00, 0xFF000000, 0xFF0000FF};
 
-        for(int i = 1; i < 10; i++){
+        for(int i = 1; i < 15; i++){
             Random random = new Random();
             PieChartBean bean = new PieChartBean();
             bean.setTitle("标题"+ i);
@@ -56,26 +67,6 @@ public class MainActivity extends AppCompatActivity {
 
         pieChart.setPieChartAdapter(adapter);
 
-        for(int j = 0; j < 10;j++){
-            PieChartBean bean = new PieChartBean();
-            bean.setTitle("新加的标题"+ j);
-            bean.setValue(50);
-            bean.setDescription("新加的描述" + j);
-            list.add(bean);
-        }
-
-//        pieChart.setBackgroundColor(Color.RED);
-//        pieChart.setIndicatorAngle(20);
-//        pieChart.setIndicatorHeight(PieChartUtils.dp2px(this, 50));
-//        pieChart.setIndicatorColor(Color.GREEN);
-//        pieChart.setEntranceAnimationDuration(200);
-//        pieChart.setOffsetAnimationDuration(800);
-//        pieChart.setOutsideStrokeWidth(PieChartUtils.dp2px(this, 20));
-//        pieChart.setOutsideStrokeColor(Color.BLUE);
-//        pieChart.setStartAngle(0);
-
-
-        new Handler().postDelayed(() -> adapter.notifyDatasetChanged(), 3000);
     }
 
 }
