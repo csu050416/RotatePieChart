ffcb3fa1f50f941e78486d1337991cf70f066e04
tengtoa <1345580079@qq.com>
2017-12-10 17:56:18 +0800

    增加动态改变数据demo

--- app/src/main/java/com/ttao/rotatepiechart/MainActivity.java
+++ app/src/main/java/com/ttao/rotatepiechart/MainActivity.java
@@ -2,8 +2,10 @@ package com.ttao.rotatepiechart;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.tt.rotate_piechart.BasePieChartAdapter;
@@ -13,7 +15,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean isAdded;
+    private BasePieChartAdapter<PieChartBean> adapter;
+    private List<PieChartBean> list = new ArrayList<>();
+    private int[] colors = new int[]{0xFFFD9033, 0xFFF98AEC, 0xFFFF3564, 0xFFF566D4, 0xFF58F4Fa,
+            0xFF3D9F33, 0xFF298AEC, 0xFFFF3F64, 0xFFF566a4, 0xFFd8F42a,
+            0xFFF490F3, 0xFF118AEC, 0xFFFFF564, 0xFFF56fD4, 0xFF5dFaFa,
+            0xFFFDF03A, 0xFF00AD00, 0xFF00FF00, 0xFFD0F0E0, 0xFF0000FF};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         RotatePieChart pieChart = findViewById(R.id.pie_chart);
         TextView textView = findViewById(R.id.text);
-
-        List<PieChartBean> list = new ArrayList<>();
-
-        int[] colors = new int[]{0xFFFD9033, 0xFFF98AEC, 0xFFFF3564, 0xFFF566D4, 0xFF58F4Fa,
-                0xFF3D9F33, 0xFF298AEC, 0xFFFF3F64, 0xFFF566a4, 0xFFd8F42a,
-                0xFFF490F3, 0xFF118AEC, 0xFFFFF564, 0xFFF56fD4, 0xFF5dFaFa,
-                0xFFFDF03A, 0xFF00AD00, 0xFF00FF00, 0xFFD0F0E0, 0xFF0000FF};
+        findViewById(R.id.btn_change_data).setOnClickListener(this);
 
         for(int i = 0; i < 15; i++){
             Random random = new Random();
@@ -38,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             list.add(bean);
         }
 
-        BasePieChartAdapter adapter = new BasePieChartAdapter<PieChartBean>(list) {
+        adapter = new BasePieChartAdapter<PieChartBean>(list) {
             @Override
             protected float getJudgeData(PieChartBean bean) {
                 return bean.getValue();
@@ -50,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            protected void onSelected(int position, PieChartBean data) {
-                String text = "value:" + data.getValue() + "\ntitle:" + data.getTitle()
-                        + "\ndescription:" + data.getDescription();
+            protected void onSelected(int position, float percentValue, PieChartBean data) {
+                String text = "percent:" + percentValue + "\nvalue:" + data.getValue()
+                        + "\ntitle:" + data.getTitle() + "\ndescription:" + data.getDescription();
                 textView.setText(text);
             }
         };
@@ -66,4 +70,32 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_change_data:
+                if (isAdded) {
+                    for (int i = 0; i < 3; i++) {
+                        list.remove(list.size() - 1);
+                    }
+                } else {
+                    for(int i = 0; i < 3; i++){
+                        Random random = new Random();
+                        PieChartBean bean = new PieChartBean();
+                        bean.setTitle("新加的条目标题"+ i);
+                        bean.setValue(random.nextInt(100) +
+                                (i + 1) * random.nextInt(50) + random.nextInt(10));
+                        bean.setDescription("新加的条目描述" + i);
+                        list.add(bean);
+                    }
+                }
+                isAdded = !isAdded;
+                ((Button)v).setText(getResources().getString(isAdded ? R.string.btn_text_deduct
+                        : R.string.btn_text_add));
+                adapter.notifyDatasetChanged();
+                break;
+            default:
+                break;
+        }
+    }
 }
